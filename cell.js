a22ada91dc470492cac7112d0bcec63303036312
Ninglin <hugo.tavares.9@gmail.com>
2016-09-03 03:24:01 +0100

    Fixed pathfinding
    Fixed neighbor discovery

--- cell.js
+++ cell.js
@@ -1,13 +1,13 @@
-function Cell(row, column, width) {
+function Cell( row, column, width ) {
 	this.visited = false;
 	this.row = row;
 	this.column = column;
 	this.width = width;
-	this.walls = [true, true, true, true];
+	this.walls = [ true, true, true, true ];
 
 
-	var canvas = document.getElementById("drawArea");
-	this.ctx = canvas.getContext('2d');
+	var canvas = document.getElementById( "drawArea" );
+	this.ctx = canvas.getContext( '2d' );
 }
 
 Cell.prototype.draw = function () {
@@ -16,15 +16,15 @@ Cell.prototype.draw = function () {
 
 	this.ctx.beginPath();
 
-	this.ctx.moveTo(x, y);
-	if (this.walls[0])
-		this.ctx.lineTo(x + this.width, y);
-	if (this.walls[1])
-		this.ctx.lineTo(x + this.width, y + this.width);
-	if (this.walls[2])
-		this.ctx.lineTo(x, y + this.width);
-	if (this.walls[3])
-		this.ctx.lineTo(x, y);
+	//this.ctx.moveTo(x, y);
+	if ( this.walls[ 0 ] )
+		this.ctx.lineTo( x + this.width, y );
+	if ( this.walls[ 1 ] )
+		this.ctx.lineTo( x + this.width, y + this.width );
+	if ( this.walls[ 2 ] )
+		this.ctx.lineTo( x, y + this.width );
+	if ( this.walls[ 3 ] )
+		this.ctx.lineTo( x, y );
 
 	this.ctx.closePath();
 
@@ -32,9 +32,9 @@ Cell.prototype.draw = function () {
 	this.ctx.stroke();
 	this.ctx.strokeStyle = null;
 
-	if (this.visited) {
-		this.ctx.fillStyle = "#7D4EC2";
-		this.ctx.fillRect(x, y, this.width, this.width);
+	if ( this.visited ) {
+		this.ctx.fillStyle = "#7d4ec2";
+		this.ctx.fillRect( x, y, this.width, this.width );
 	}
 };
 
@@ -42,22 +42,55 @@ Cell.prototype.visit = function () {
 	this.visited = true;
 	var neighbours = [];
 
-	var top = grid[getGridIndex(this.row - 1, this.column)];
-	var right = grid[getGridIndex(this.row, this.column + 1)];
-	var bottom = grid[getGridIndex(this.row + 1, this.column)];
-	var left = grid[getGridIndex(this.row, this.column - 1)];
+	var top = grid[ getGridIndex( this.row - 1, this.column ) ];
+	var right = grid[ getGridIndex( this.row, this.column + 1 ) ];
+	var bottom = grid[ getGridIndex( this.row + 1, this.column ) ];
+	var left = grid[ getGridIndex( this.row, this.column - 1 ) ];
 
-	if (top && !top.visited)
-		neighbours.push(top);
-	if (right && !right.visited)
-		neighbours.push(right);
-	if (bottom && !bottom.visited)
-		neighbours.push(bottom);
-	if (left && !left.visited)
-		neighbours.push(left);
+	if ( top && !top.visited )
+		neighbours.push( top );
+	if ( right && !right.visited )
+		neighbours.push( right );
+	if ( bottom && !bottom.visited )
+		neighbours.push( bottom );
+	if ( left && !left.visited )
+		neighbours.push( left );
 
-	if (neighbours.length > 0)
-		return neighbours[Math.floor(Math.random(0, neighbours.length))];
-	else
+	if ( neighbours.length > 0 ) {
+		var nextIndex = Math.floor( Math.random() * ( neighbours.length + 1 ) );
+		return neighbours[ nextIndex ];
+	} else
 		return undefined;
+};
+
+Cell.prototype.collapseWall = function ( neighbor ) {
+	console.log( this );
+	var xDirection = this.column - neighbor.column;
+	if ( xDirection === 1 ) {
+		//the neighbor is to the left
+		this.walls[ 3 ] = false;
+		neighbor.walls[ 1 ] = false;
+	} else if ( xDirection === -1 ) {
+		//the neighbor is to the left
+		this.walls[ 1 ] = false;
+		neighbor.walls[ 3 ] = false;
+	}
+
+	var yDirection = this.row - neighbor.row;
+	if ( yDirection === 1 ) {
+		//the neighbor is above
+		this.walls[ 0 ] = false;
+		neighbor.walls[ 2 ] = false;
+	} else if ( yDirection === -1 ) {
+		//the neighbor is below
+		this.walls[ 2 ] = false;
+		neighbor.walls[ 0 ] = false;
+	}
+};
+
+Cell.prototype.highlight = function () {
+	var x = this.row * this.width;
+	var y = this.column * this.width;
+	this.ctx.fillStyle = "#5a1480"
+	this.ctx.fillRect( x, y, this.width - 2, this.width - 2 );
 };
--- index.js
+++ index.js
@@ -1,65 +1,70 @@
 var grid = [];
 var rows, columns;
 
-(function (window, document, undefined) {
-	var cellWidth = 40;
+( function ( window, document, undefined ) {
+	var cellWidth = 80;
 	var current, fps, fpsInterval, ellapsed, now, then;
 
-	document.addEventListener('DOMContentLoaded',
+	document.addEventListener( 'DOMContentLoaded',
 		function () {
-			setup(5);
+			setup( 10 );
 			draw();
-		});
+		} );
 
-	function createCanvas(width, height) {
-		var body = document.querySelector('body');
-		if (body) {
-			body.innerHTML = '<canvas id="drawArea" width="' + width + '" height="' + height + '" />'
-		}
-		rows = Math.floor(width / cellWidth);
-		columns = Math.floor(width / cellWidth);
-	}
 
-	function initGrid() {
-		for (var i = 0; i < rows; i++) {
-			for (var j = 0; j < columns; j++) {
-				var cell = new Cell(i, j, cellWidth);
-				grid.push(cell);
-			}
-		}
-	}
-
-	function setup(fps) {
-		createCanvas(400, 400);
+	function setup( fps ) {
+		createCanvas( 400, 400 );
 		initGrid();
-		current = grid[0];
+		current = grid[ 0 ];
 		fpsInterval = 1000 / fps;
 		then = Date.now();
 	}
 
 	function draw() {
-		window.setTimeout(fpsInterval);
-		window.requestAnimationFrame(draw);
 		now = Date.now();
 		ellapsed = now - then;
 
-		if (ellapsed > fpsInterval) {
-
-			then = now - (ellapsed % fpsInterval);
-			for (let cell of grid) {
+		if ( ellapsed > fpsInterval ) {
+			for ( let cell of grid ) {
 				cell.draw();
 			}
 
 			var next = current.visit();
-			if (next)
+			current.highlight();
+			if ( next ) {
+				// next.visited = true;
+				current.collapseWall( next );
+
 				current = next;
+			}
+
+			then = now - ( ellapsed % fpsInterval );
+		}
+		window.requestAnimationFrame( draw );
+	}
+
+	function createCanvas( width, height ) {
+		var body = document.querySelector( 'body' );
+		if ( body ) {
+			body.innerHTML = '<canvas id="drawArea" width="' + width + '" height="' + height + '" />'
+		}
+		rows = Math.floor( width / cellWidth );
+		columns = Math.floor( width / cellWidth );
+	}
+
+	function initGrid() {
+		for ( var i = 0; i < rows; i++ ) {
+			for ( var j = 0; j < columns; j++ ) {
+				var cell = new Cell( i, j, cellWidth );
+				grid.push( cell );
+			}
 		}
 	}
 
-}(window, document))
+}( window, document ) )
 
-function getGridIndex(row, col) {
-	if (row < 0 || col < 0 || row > rows - 1 || col > columns - 1)
-		return undefined;
-	return row + col * columns;
+function getGridIndex( row, col ) {
+	if ( row < 0 || col < 0 || row > rows - 1 || col > columns - 1 )
+		return -1;
+	return row * columns + col;
 }
