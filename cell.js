8bc44e3f344e03a3ee4a848ead41e0cc7a1db093
Hugo Tavares <hugo.tavares@tekever.com>
2016-09-06 15:48:44 +0100

    Fixed drawing issue and added ternjs line config

--- cell.js
+++ cell.js
@@ -1,90 +1,101 @@
-function Cell( row, column, width ) {
+function Cell(row, column, width) {
 	this.visited = false;
 	this.row = row;
 	this.column = column;
 	this.width = width;
-	this.walls = [ true, true, true, true ];
+	this.walls = [true, true, true, true];
 
 
-	var canvas = document.getElementById( "drawArea" );
-	this.ctx = canvas.getContext( '2d' );
+	var canvas = document.getElementById("drawArea");
+	this.ctx = canvas.getContext('2d');
 }
 
 Cell.prototype.draw = function () {
-	var x = this.row * this.width;
-	var y = this.column * this.width;
+	var x = this.column * this.width;
+	var y = this.row * this.width;
 
 	this.ctx.beginPath();
 
-	//this.ctx.moveTo(x, y);
-	if ( this.walls[ 0 ] )
-		this.ctx.lineTo( x + this.width, y );
-	if ( this.walls[ 1 ] )
-		this.ctx.lineTo( x + this.width, y + this.width );
-	if ( this.walls[ 2 ] )
-		this.ctx.lineTo( x, y + this.width );
-	if ( this.walls[ 3 ] )
-		this.ctx.lineTo( x, y );
-
-	this.ctx.closePath();
+	this.ctx.moveTo(x, y);
+	//Top Wall
+	if (this.walls[0])
+		this.ctx.lineTo(x + this.width, y);
+	else
+		this.ctx.moveTo(x + this.width, y);
+	//Right Wall
+	if (this.walls[1])
+		this.ctx.lineTo(x + this.width, y + this.width);
+	else
+		this.ctx.moveTo(x + this.width, y + this.width);
+	//Bottom Wall
+	if (this.walls[2])
+		this.ctx.lineTo(x, y + this.width);
+	else
+		this.ctx.moveTo(x, y + this.width);
+	//Left Wall
+	if (this.walls[3])
+		this.ctx.lineTo(x, y);
+	else
+		this.ctx.moveTo(x, y);
 
 	this.ctx.strokeStyle = "#CCC";
 	this.ctx.stroke();
-	this.ctx.strokeStyle = null;
 
-	if ( this.visited ) {
+	if (this.visited) {
 		this.ctx.fillStyle = "#7d4ec2";
-		this.ctx.fillRect( x, y, this.width, this.width );
+		this.ctx.fillRect(x, y, this.width, this.width);
 	}
 };
 
-Cell.prototype.visit = function () {
-	this.visited = true;
+Cell.prototype.getNextNeighbor = function () {
 	var neighbours = [];
 
-	var top = grid[ getGridIndex( this.row - 1, this.column ) ];
-	var right = grid[ getGridIndex( this.row, this.column + 1 ) ];
-	var bottom = grid[ getGridIndex( this.row + 1, this.column ) ];
-	var left = grid[ getGridIndex( this.row, this.column - 1 ) ];
+	var top = grid[getGridIndex(this.row - 1, this.column)];
+	var right = grid[getGridIndex(this.row, this.column + 1)];
+	var bottom = grid[getGridIndex(this.row + 1, this.column)];
+	var left = grid[getGridIndex(this.row, this.column - 1)];
 
-	if ( top && !top.visited )
-		neighbours.push( top );
-	if ( right && !right.visited )
-		neighbours.push( right );
-	if ( bottom && !bottom.visited )
-		neighbours.push( bottom );
-	if ( left && !left.visited )
-		neighbours.push( left );
+	if (top && !top.visited)
+		neighbours.push(top);
+	if (right && !right.visited)
+		neighbours.push(right);
+	if (bottom && !bottom.visited)
+		neighbours.push(bottom);
+	if (left && !left.visited)
+		neighbours.push(left);
 
-	if ( neighbours.length > 0 ) {
-		var nextIndex = Math.floor( Math.random() * ( neighbours.length + 1 ) );
-		return neighbours[ nextIndex ];
+	if (neighbours.length > 0) {
+		var nextIndex = Math.floor(Math.random() * (neighbours.length + 1));
+		return neighbours[nextIndex];
 	} else
 		return undefined;
 };
 
-Cell.prototype.collapseWall = function ( neighbor ) {
-	console.log( this );
+Cell.prototype.collapseWall = function (neighbor) {
+	console.log("Current");
+	console.log(this);
+	console.log("Next");
+	console.log(neighbor);
 	var xDirection = this.column - neighbor.column;
-	if ( xDirection === 1 ) {
+	if (xDirection === 1) {
 		//the neighbor is to the left
-		this.walls[ 3 ] = false;
-		neighbor.walls[ 1 ] = false;
-	} else if ( xDirection === -1 ) {
+		this.walls[3] = false;
+		neighbor.walls[1] = false;
+	} else if (xDirection === -1) {
 		//the neighbor is to the left
-		this.walls[ 1 ] = false;
-		neighbor.walls[ 3 ] = false;
+		this.walls[1] = false;
+		neighbor.walls[3] = false;
 	}
 
 	var yDirection = this.row - neighbor.row;
-	if ( yDirection === 1 ) {
+	if (yDirection === 1) {
 		//the neighbor is above
-		this.walls[ 0 ] = false;
-		neighbor.walls[ 2 ] = false;
-	} else if ( yDirection === -1 ) {
+		this.walls[0] = false;
+		neighbor.walls[2] = false;
+	} else if (yDirection === -1) {
 		//the neighbor is below
-		this.walls[ 2 ] = false;
-		neighbor.walls[ 0 ] = false;
+		this.walls[2] = false;
+		neighbor.walls[0] = false;
 	}
 };
 
@@ -92,5 +103,5 @@ Cell.prototype.highlight = function () {
 	var x = this.row * this.width;
 	var y = this.column * this.width;
 	this.ctx.fillStyle = "#5a1480"
-	this.ctx.fillRect( x, y, this.width - 2, this.width - 2 );
+	this.ctx.fillRect(x, y, this.width, this.width);
 };
--- index.js
+++ index.js
@@ -1,70 +1,101 @@
 var grid = [];
-var rows, columns;
+var rows, columns, ctx;
 
-( function ( window, document, undefined ) {
+(function (window, document, undefined) {
 	var cellWidth = 80;
-	var current, fps, fpsInterval, ellapsed, now, then;
+	var gridSize = 400;
+	var current, fps, fpsInterval, ellapsed, now, then, canvas;
 
-	document.addEventListener( 'DOMContentLoaded',
+	document.addEventListener('DOMContentLoaded',
 		function () {
-			setup( 10 );
+			setup(5);
 			draw();
-		} );
+		});
 
 
-	function setup( fps ) {
-		createCanvas( 400, 400 );
-		initGrid();
-		current = grid[ 0 ];
+	function setup(fps) {
+		canvas = document.getElementById("drawArea");
+		ctx = canvas.getContext('2d');
 		fpsInterval = 1000 / fps;
 		then = Date.now();
+
+		createCanvas(400, 400);
+		for (var i = 0; i < rows; i++) {
+			for (var j = 0; j < columns; j++) {
+				var cell = new Cell(i, j, cellWidth);
+				grid.push(cell);
+			}
+		}
+
+		current = grid[0];
 	}
 
 	function draw() {
 		now = Date.now();
 		ellapsed = now - then;
 
-		if ( ellapsed > fpsInterval ) {
-			for ( let cell of grid ) {
+		if (ellapsed > fpsInterval) {
+			clear();
+			for (let cell of grid) {
 				cell.draw();
 			}
 
-			var next = current.visit();
+			current.visited = true;
 			current.highlight();
-			if ( next ) {
-				// next.visited = true;
-				current.collapseWall( next );
+			var next = current.getNextNeighbor();
+			if (next) {
+				next.visited = true;
+
+				//current.collapseWall(next);
+				removeWalls(current, next);
 
 				current = next;
 			}
 
-			then = now - ( ellapsed % fpsInterval );
+			then = now - (ellapsed % fpsInterval);
 		}
-		window.requestAnimationFrame( draw );
+		window.requestAnimationFrame(draw);
 	}
 
-	function createCanvas( width, height ) {
-		var body = document.querySelector( 'body' );
-		if ( body ) {
+	function createCanvas(width, height) {
+		var body = document.querySelector('body');
+		if (body) {
 			body.innerHTML = '<canvas id="drawArea" width="' + width + '" height="' + height + '" />'
 		}
-		rows = Math.floor( width / cellWidth );
-		columns = Math.floor( width / cellWidth );
+		rows = Math.floor(height / cellWidth);
+		columns = Math.floor(width / cellWidth);
 	}
 
-	function initGrid() {
-		for ( var i = 0; i < rows; i++ ) {
-			for ( var j = 0; j < columns; j++ ) {
-				var cell = new Cell( i, j, cellWidth );
-				grid.push( cell );
-			}
+	function clear() {
+		ctx.save();
+
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		ctx.restore();
+	}
+
+	function removeWalls(a, b) {
+		var x = a.i - b.i;
+		if (x === 1) {
+			a.walls[3] = false;
+			b.walls[1] = false;
+		} else if (x === -1) {
+			a.walls[1] = false;
+			b.walls[3] = false;
+		}
+		var y = a.j - b.j;
+		if (y === 1) {
+			a.walls[0] = false;
+			b.walls[2] = false;
+		} else if (y === -1) {
+			a.walls[2] = false;
+			b.walls[0] = false;
 		}
 	}
+}(window, document))
 
-}( window, document ) )
-
-function getGridIndex( row, col ) {
-	if ( row < 0 || col < 0 || row > rows - 1 || col > columns - 1 )
+function getGridIndex(row, col) {
+	if (row < 0 || col < 0 || row > rows - 1 || col > columns - 1)
 		return -1;
 	return row * columns + col;
 }
